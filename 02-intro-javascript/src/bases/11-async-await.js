472b8efaeae41dce8e78a6ab11c7b6a972fb7623
guidomatos <guido.matos.88@gmail.com>
2021-02-01 12:25:34 -0500

    Async - Await

---
+++ 02-intro-javascript/src/bases/11-async-await.js
@@ -0,0 +1,26 @@
+
+
+// const getImagenPromesa = () =>  new Promise ( resolve => resolve('http://qwefkqwefjnq.com')  );
+
+const getImagen = async() => {
+
+    try {
+
+        const apiKey = 'RqK4YK4KN9L2bAUnn816OMy2ph0LCz2d'; 
+        const resp = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`);
+        const { data } = await resp.json();
+
+        const { url } = data.images.original;
+
+        const img = document.createElement('img');
+        img.src = url;
+        document.body.append( img );
+
+    } catch (error) {
+        // manejo del error
+        console.error(error);
+    }
+    
+}
+
+getImagen();
