45c0a054ef2a706d43011b89690c3900d41b193a
guidomatos <guido.matos.88@gmail.com>
2021-02-01 11:55:59 -0500

    Fetch API

---
+++ 02-intro-javascript/src/bases/10-fetch.js
@@ -0,0 +1,17 @@
+const apiKey = 'RqK4YK4KN9L2bAUnn816OMy2ph0LCz2d';
+
+const peticion = fetch(`http://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`);
+
+peticion
+    .then( resp => resp.json() )
+    .then( ({ data }) => {
+        // console.log( data.images.original.url )  
+        const { url } = data.images.original;
+
+        const img = document.createElement('img');
+        img.src = url;
+
+        document.body.append( img );
+
+    })
+    .catch( console.warn );
